1634b28187268169b49510c563fa5db001a3e362
Fernando Aponte <faponte1230@yahoo.com>
2023-10-24 23:48:08 -0400

    create arcades controller actions

--- app/controllers/arcades_controller.rb
+++ app/controllers/arcades_controller.rb
@@ -1,10 +1,24 @@
 class ArcadesController < ApplicationController
+  before_action :authorize, only: [index]
+
   def create
+    arcade = Arcade.create!(arcade_params)
+    render json: arcade, status: :created
   end
 
   def index
+    arcades = Arcade.all 
+    render json: arcades, status: :ok
   end
 
   def show
+    arcade = Arcade.find(params[:id])
+    render json: arcade
+  end
+
+  private
+
+  def arcade_params
+    params.permit(:name, :num_of_games, :img_url)
   end
 end
