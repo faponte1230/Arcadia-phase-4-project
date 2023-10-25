9f21ffee8b414f4f0f30ddd1328f655b23b763a3
Fernando Aponte <faponte1230@yahoo.com>
2023-10-24 22:51:54 -0400

    create user validations and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    has_secure_password
+    has_many :reviews
+    has_many :arcades, through: :reviews 
+    
+    validates :username, :password, :password_confirmation, presence: true
 end
