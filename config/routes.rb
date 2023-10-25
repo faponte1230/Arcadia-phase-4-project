b16ff43e7c18ab7f621d16a1f70cc54743fb3af5
Fernando Aponte <faponte1230@yahoo.com>
2023-10-24 22:43:09 -0400

    created models

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   
+  get 'users/create'
+  get 'users/show'
+  get 'arcades/create'
+  get 'arcades/index'
+  get 'arcades/show'
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
