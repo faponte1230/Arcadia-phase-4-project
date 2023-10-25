92e2e8796e4ae9036f635e892f6784e37c5ad40d
Fernando Aponte <faponte1230@yahoo.com>
2023-10-24 22:54:40 -0400

    create arcade validations and associations

--- app/models/arcade.rb
+++ app/models/arcade.rb
@@ -1,2 +1,8 @@
 class Arcade < ApplicationRecord
+    has_many :users
+    has_many :reviews, through: :users
+
+    validates :name, presence: true
+    validates :img_url, presence: true
+    validates :num_of_games, numericality: {only_integer: true}
 end
