c22e12a4fe366543e8b46e3a3659d081406a9bfd
Fernando Aponte <faponte1230@yahoo.com>
2023-10-25 00:45:36 -0400

    create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Arcade.destroy_all
+Review.destroy_all
+
+puts("Data Deleted..")
+puts("Data Loading..")
+
+#USER
+a = User.create(username: "Fernando", password: "123", password_confirmation: "123")
+b = User.create(username: "Josh", password: "123", password_confirmation: "123")
+c = User.create(username: "Justin", password: "123", password_confirmation: "123")
+
+#Arcade 
+arc1 = Arcade.create(name: "Barcade", num_of_games: 25, img_url: "https://parknewhaven.com/wp-content/uploads/2016/10/deal-barcade.jpg")
+arc2 = Arcade.create(name: "Bowlero", num_of_games: 10, img_url: "https://addisontexas.net/sites/default/files/imageattachments/busdir/8361/bowlero.png")
+arc3 = Arcade.create(name: "Dave'n'Busters", num_of_games: 50, img_url: "https://live.staticflickr.com/2904/14110169303_98984fc612_b.jpg")
